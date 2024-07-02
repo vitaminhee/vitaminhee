eac002501cd99bbbcce4dbd12371612a99b37509
USRE <USRE@DESKTOP-540HPTU>
2024-07-02 15:40:30 +0900

    ok

--- webjjang_servlet/src/main/java/com/webjjang/image/service/ImageViewService.java
+++ webjjang_servlet/src/main/java/com/webjjang/image/service/ImageViewService.java
@@ -1,32 +1,24 @@
 package com.webjjang.image.service;
 
-import com.webjjang.board.dao.BoardDAO;
-import com.webjjang.board.vo.BoardVO;
+import com.webjjang.image.dao.ImageDAO;
+import com.webjjang.image.vo.ImageVO;
 import com.webjjang.main.dao.DAO;
 import com.webjjang.main.service.Service;
 
 public class ImageViewService implements Service {
 
-	private BoardDAO dao;
+	private ImageDAO dao;
 	
 	// dao setter(집어놓)
 	public void setDAO(DAO dao) {
-		this.dao = (BoardDAO) dao; // 자식 class이므로 캐스팅
+		this.dao = (ImageDAO) dao; // 자식 class이므로 캐스팅
 	}
 	
 	@Override
-	public BoardVO service(Object obj) throws Exception {
-		// 넘어오는 데이터의 구조 obj - Long[] : obj[0] - no, obj[1] - inc
-		Long[] objs = (Long[]) obj; // 데이터를 넣은 때 Long[]로 저장했어야만 한다.
-		Long no = objs[0];
-		Long inc = objs[1];
-		// DB board에서 조회수 1증가 하고 글보기 데이터 가져와서 리턴
-		BoardDAO dao = new BoardDAO();
-		// 1. 조회수 1증가 : inc == 1
-		if(inc == 1) dao.increase(no);
-		// DB 처리는 DAO에서 처리 - BoardDAO.list()
-		// BoardController - (Execute) - [BoardListService] - BoardDAO.view()
-		return dao.view(no);
+	public ImageVO service(Object obj) throws Exception {
+		// DB 처리는 DAO에서 처리 - ImageDAO.view()
+		// ImageController - (Execute) - [ImageViewService] - ImageDAO.view()
+		return dao.view((Long) obj);
 	}
 
 }
--- webjjang_servlet/src/main/java/com/webjjang/main/controller/Init.java
+++ webjjang_servlet/src/main/java/com/webjjang/main/controller/Init.java
@@ -86,7 +86,7 @@ public class Init {
 				serviceMap.put("/image/delete.do", new ImageDeleteService());
 				// 조립 dao->service
 				serviceMap.get("/image/list.do").setDAO(daoMap.get("imageDAO"));
-				serviceMap.get("/image/view.do").setDAO(daoMap.get("boardDAO"));
+				serviceMap.get("/image/view.do").setDAO(daoMap.get("imageDAO"));
 				serviceMap.get("/image/write.do").setDAO(daoMap.get("imageDAO"));
 				serviceMap.get("/image/update.do").setDAO(daoMap.get("boardDAO"));
 				serviceMap.get("/image/delete.do").setDAO(daoMap.get("boardDAO"));
